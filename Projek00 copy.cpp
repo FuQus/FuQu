943fd939fbd705738d4109187dae7474a92bb8a4
FuQus <mubdilazuard1105@gmail.com>
2024-06-06 14:27:38 +0800

    menu memilih

--- Projek00 copy.cpp
+++ Projek00 copy.cpp
@@ -7,8 +7,8 @@ using namespace std;
 
 void dmenu(){
 system("cls");
-cout<<"Aplikasi Tempat Menu"<<"\n";
-cout<<"1.Menu pertama"<<"\n";
+cout<<"Aplikasi Tempat Menu!"<<"\n";
+cout<<"1.Menu pertama!"<<"\n";
 cout<<"2.Menu kedua"<<"\n";
 cout<<"3.Menu ketiga"<<"\n";
 cout<<"4.Menu keempat"<<"\n";
@@ -18,6 +18,31 @@ cout<<"masukkan angka :";
 
 void mPertama(){
     system("cls");
- cout<<"hallo saya menu pertama!";
+ cout<<"hallo saya menu pertama!!";
  getch ();
+}
+
+int main (){
+char pl;
+do{
+    dmenu();
+    pl = getch ();
+
+switch (pl)
+{
+    case '1':
+    mPertama();
+    break;
+
+    case '5':
+    break;
+
+    default:
+    system("cls");
+    cout<<"pilihan tidak tersedia!";
+    getch ();
+    break;
+}
+} while (pl !='5');
+return 0;
 }
